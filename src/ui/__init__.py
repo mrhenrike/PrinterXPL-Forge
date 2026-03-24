e2e8a5171b817b2db4a551a36d9c8dfeb26be2f4
André Henrique <mrhenrike@users.noreply.github.com>
2026-03-24 20:36:10 -0300

    feat(ux): add interactive guided menu, spinner, section headers, next-steps; bump v3.4.2

---
+++ src/ui/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author    : Andre Henrique (@mrhenrike)
+# GitHub    : https://github.com/mrhenrike
+# LinkedIn  : https://linkedin.com/in/mrhenrike
+# X/Twitter : https://x.com/mrhenrike
